af5ba1adc7d357ae5693b8be3ff90fa6ad044af9
rohitjosephalex <119044831+rohitjosephalex@users.noreply.github.com>
2023-12-23 04:29:01 +0530

    Update edit.js

--- services/edit.js
+++ services/edit.js
@@ -40,21 +40,6 @@ const reorderPDF=async(inputPath, outputPath, orderArray,pageArray) =>{
     console.error('Error:', error.message);
   }
 }
-const array1 = [3, 1, 2, 4, 5, 7, 11, 6, 8, 10, 9, 12];
-const array2 = [1, 2, 3, 5, 7, 9, 10, 12];
 
-// const newArray = array1.filter((element) => array2.includes(element));
-
-// console.log(newArray);
-// Example usage
-const inputPDFPath = './services/DOP_BulkCustomerIntegration_v8_1.pdf';
-const outputPDFPath = './output.pdf';
-const pageOrder = {
-  '1': 3,
-  '2': 1,
-  '3': 5, // No replacement for page 3
-  '4': 2,
-  '5': 0
-};
 // reorderPDF(inputPDFPath,outputPDFPath,array1,array2);
 module.exports={reorderPDF}
